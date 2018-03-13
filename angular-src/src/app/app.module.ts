f84f9fdc0676a7f0fb746a31fdedc56230e82294
paulchin <paulchin20@yahoo.com>
2018-03-14 00:01:35 +0800

    Completed Angular 5 pre-built

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -14,13 +14,14 @@ import { ValidateService } from './services/validate.service';
 import { AuthService } from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages';
 import { HttpModule } from '@angular/http';
+import { AuthGuard } from './guards/auth.guard';
 
 const appRoutes: Routes = [
   {path:'',component:HomeComponent},
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
-  {path:'dashboard',component:DashboardComponent},
-  {path:'profile',component:ProfileComponent}
+  {path:'dashboard',component:DashboardComponent, canActivate:[AuthGuard]},
+  {path:'profile',component:ProfileComponent, canActivate:[AuthGuard]}
 ]
 
 @NgModule({
@@ -40,7 +41,7 @@ const appRoutes: Routes = [
     FlashMessagesModule.forRoot(),
     HttpModule
   ],
-  providers: [ValidateService, AuthService],
+  providers: [ValidateService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
