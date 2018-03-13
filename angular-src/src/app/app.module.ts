34774710b9d0af94ebbd7e1bcd92e95631b4fbb2
paulchin <paulchin20@yahoo.com>
2018-03-13 18:00:47 +0800

    Added Navbar, Home Page, Login Page and Register Page

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { FormsModule } from '@angular/forms';
+import { ValidateService } from './services/validate.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 const appRoutes: Routes = [
   {path:'',component:HomeComponent},
@@ -30,9 +33,11 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
